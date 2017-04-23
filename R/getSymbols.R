86a3dd6ec723dda96e2b9b9d6462b1016dfa16ba
arbuzovv <arbuzov1989@gmail.com>
2017-04-23 17:59:43 +0500

    fix issue
    Finam moved to https from http

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -616,8 +616,8 @@ fr <- xts(apply(as.matrix(fr[,(5:10)]),2, as.numeric), as.Date(strptime(fr[,3], 
 "loadStockList" <-
 function (verbose = FALSE){
     stocklist.URL = 'https://www.finam.ru/cache/icharts/icharts.js'
-    tmp <- tempfile()
-    download.file(stocklist.URL, destfile=tmp,quiet=!verbose)
+	tmp <- tempfile()
+	curl_download(stocklist.URL, tmp, quiet=!verbose)
     fr <- readLines(con = tmp, warn=FALSE)
     unlink(tmp)
     ids <- sub("var .*?= \\[", "", fr[1])
