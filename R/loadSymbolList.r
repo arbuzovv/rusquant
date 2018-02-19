95256dae38f44052b8bf2bce74bb9c9dd63e343f
arbuzovv <arbuzov1989@gmail.com>
2018-02-19 11:35:02 +0500

    fix bugs

--- R/loadSymbolList.r
+++ R/loadSymbolList.r
@@ -3,16 +3,14 @@
 
 #function retrives All avalible instruments of exchange
 "loadSymbolList" <- function #S3 function (Poloniex is a class of first argument)
-(src='kraken'
+(src='kraken',
  verbose=FALSE,
  auto.assign=FALSE,
  ...)
 {
         importDefaults("loadSymbolList"); #rewrite default values if specified by setDefaults
         env <- environment()
-        
-        if(missing(verbose)) verbose <- TRUE
-       
+          
         downloadUrl <- 'https://api.kraken.com/0/public/AssetPairs'
                 
         tmp <- tempfile()
@@ -33,8 +31,7 @@
 					rawdata_m <- rbind(rawdata_m,data.frame(raw_currency[1],raw_currency[2],raw_currency[3],raw_currency[4],raw_currency[5],raw_currency[6],raw_currency[7],raw_currency[8],raw_currency[9]))
 			}	
 		}
-                
-             	
+  	
                 result <-paste('symbol_list_',toupper(gsub('\\^','',src)),sep='_')
                 if(auto.assign){
                 
