30a6cf2979aa04b352bb95593d4a370b148b9362
arbuzovv <arbuzov1989@gmail.com>
2018-04-18 11:23:56 +0500

    Additional exchanges for LoadSymbolList

--- R/loadSymbolList.r
+++ R/loadSymbolList.r
@@ -3,66 +3,72 @@
 
 #function retrives All avalible instruments of exchange
 "loadSymbolList" <- function #S3 function (Poloniex is a class of first argument)
-(src='kraken',
+(src='poloniex',
  verbose=FALSE,
  auto.assign=FALSE,
  ...)
 {
         importDefaults("loadSymbolList"); #rewrite default values if specified by setDefaults
         env <- environment()
-         
-		if(src == 'kraken')
-			downloadUrl <- 'https://api.kraken.com/0/public/AssetPairs'
-		if(src == 'poloniex')		
-			downloadUrl <- 'https://poloniex.com/public?command=returnTicker'
-                
-        tmp <- tempfile()
-		if(verbose == TRUE) print(paste('Downloading file from',downloadUrl))
-        download.file(downloadUrl, destfile = tmp, quiet = TRUE,method = 'curl') #get JSON object
-		date_time <- Sys.time()
-        rawdata <- readLines(tmp) #read raw data from file
-        #convert to JSON
-		rawdata <- fromJSON(rawdata)
-		
-		if(src == 'kraken')
-		{
-		if(length(rawdata$error)==0)
-		{
-			for(i in 1:length(names(rawdata[[2]])))
-			{
-				raw_currency <- rawdata[[2]][[i]]		
-				if(i == 1)
-					rawdata_m <- data.frame(raw_currency[1],raw_currency[2],raw_currency[3],raw_currency[4],raw_currency[5],raw_currency[6],raw_currency[7],raw_currency[8],raw_currency[9])
-				if(i != 1)
-					rawdata_m <- rbind(rawdata_m,data.frame(raw_currency[1],raw_currency[2],raw_currency[3],raw_currency[4],raw_currency[5],raw_currency[6],raw_currency[7],raw_currency[8],raw_currency[9]))
-			}	
-		}
-		}
-		if(src == 'poloniex')
-		{
-			for(i in 1:length(names(rawdata)))
-			{
-				raw_currency <- rawdata[[i]]		
-				names(rawdata)[i]
-				if(i == 1)
-					rawdata_m <- data.frame(names(rawdata)[i],raw_currency[1],raw_currency[2],raw_currency[3],raw_currency[4],raw_currency[5],raw_currency[6],raw_currency[7],raw_currency[8])
-				if(i != 1)
-					rawdata_m <- rbind(rawdata_m,data.frame(names(rawdata)[i],raw_currency[1],raw_currency[2],raw_currency[3],raw_currency[4],raw_currency[5],raw_currency[6],raw_currency[7],raw_currency[8]))
-			}
-			rawdata_m[,2:ncol(rawdata_m)] <- apply(rawdata_m[,2:ncol(rawdata_m)],2,function(x) as.numeric(as.character(x)))
-			rawdata_m[,1] <- as.character(rawdata_m[,1])
-			names(rawdata_m)[1] <- 'Symbol'
-		}
-		
-                result <-paste('symbol_list_',toupper(gsub('\\^','',src)),sep='_')
-                if(auto.assign){
-                
-                        assign(result, rawdata_m,globalenv())
-				}
-        
-        if(auto.assign){
-                return(result)
-        }
-        
-        return(rawdata_m)
+		src <- tolower(src)
+  
+  ## choose exchange       
+  if(src == 'kraken')
+	downloadUrl <- 'https://api.kraken.com/0/public/AssetPairs'
+
+  if(src == 'poloniex')		
+	downloadUrl <- 'https://poloniex.com/public?command=returnTicker'
+						
+  if (src == "binance")
+    downloadUrl <- "https://api.binance.com/api/v1/ticker/allPrices"
+
+  if (src == "bitfinex")
+    downloadUrl <- "https://api.bitfinex.com/v1/symbols"
+
+  if (src == "bitflyer")
+    downloadUrl <- "https://api.bitflyer.com/v1/getmarkets/usa"
+
+  if (src == "bitstamp")
+    downloadUrl <- "https://www.bitstamp.net/api/v2/trading-pairs-info/"
+
+  if (src == "bttrex")
+    downloadUrl <- "https://bittrex.com/api/v1.1/public/getmarkets"
+
+  if (src == "cex")
+    downloadUrl <- "https://cex.io/api/currency_limits"
+
+  if (src == "gate")
+    downloadUrl <- "http://data.gate.io/api2/1/pairs"
+
+  if (src == "gatecoin")
+    downloadUrl <- "https://api.gatecoin.com/Public/LiveTickers"
+
+  if (src == "gdax")
+    downloadUrl <- "https://api.gdax.com/products/"
+
+  if(src == "gemini")
+    downloadUrl <- "https://api.gemini.com/v1/symbols"
+	
+  if (src == "hitbtc")
+    downloadUrl <- "https://api.hitbtc.com/api/2/public/symbol"
+
+  if (src == "liqui")
+    downloadUrl <- "https://api.liqui.io/api/3/info"
+
+  if (src == "lykke") 
+    downloadUrl <- "https://hft-api.lykke.com/api/AssetPairs"
+
+  if (src == "xbtce") 
+    downloadUrl <- "https://cryptottlivewebapi.xbtce.net:8443/api/v1/public/symbol"
+
+#download and parse data			
+rawdata_m <- jsonlite::fromJSON(downloadUrl, simplifyVector = TRUE)			
+			
+result <-paste('symbol_list_',toupper(gsub('\\^','',src)),sep='_')			
+if(auto.assign)                
+	{
+    assign(result, rawdata_m,globalenv())			
+	return(result)
+	}										    
+return(rawdata_m)
 }
