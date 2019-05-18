4797d9249d4dc95d86ca6abe7487a6624bc84680
Vyacheslav Arbuzov <arbuzov1989@gmail.com>
2019-05-18 09:50:46 -0500

    .

--- R/openOrder.r
+++ R/openOrder.r
@@ -1,5 +1,7 @@
 openOrder <- function(Connection, Order) 
 {
+  if(exists('Order'))
+  {
         url <- "https://hft-api.lykke.com/api/Orders/limit"
         resp <- httr::POST(url,
                            body = list(AssetPairId = Order$Symbol,
@@ -9,4 +11,5 @@ openOrder <- function(Connection, Order)
                            httr::add_headers(`api-key` = conn$key),
                            encode = "json")
         return(resp)
+  }
 }
