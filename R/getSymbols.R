07d03626d389d90cc7178d32a0be7c09d3d1e8f4
Vyacheslav Arbuzov <arbuzov1989@gmail.com>
2020-01-29 18:40:36 +0500

    2020 big update

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -348,141 +348,155 @@ for(i in 1:length(Symbols)) {
 }
 
 "getSymbols.Finam" <-
-function(Symbols,env,return.class='xts',index.class='Date',
-         from='2007-01-01',
-         to=Sys.Date(),
-         adjust=FALSE,
-         period='day',
-         ...)
-{
-     importDefaults("getSymbols.Finam")
-     this.env <- environment()
-     for(var in names(list(...))) {
-        # import all named elements that are NON formals
-        assign(var, list(...)[[var]], this.env)
-     }
-
-     default.return.class <- return.class
-     default.from <- from
-     default.to <- to
-
-     if(missing(verbose)) verbose <- FALSE
-     if(missing(auto.assign)) auto.assign <- FALSE
-
-     p <- 0
-
-     if ("tick" == period) p <- 1
-     if ("1min" == period) p <- 2
-     if ("5min" == period) p <- 3
-     if ("10min" == period) p <- 4
-     if ("15min" == period) p <- 5
-     if ("30min" == period) p <- 6
-     if ("hour" == period) p <- 7
-     if ("day" == period) p <- 8
-     if ("week" == period) p <- 9
-     if ("month" == period) p <- 10
-
-     if (p==0) {
-        message(paste("Unkown period ", period))
-     }	 
-     finam.HOST <- 'export.finam.ru'
-     finam.URL <- "/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=1&MSOR=0&sep=1&sep2=1&at=1&"
-
-     if (!exists("finam.stock.list")){
-        finam.stock.list <- loadStockList()
-        assign('finam.stock.list', finam.stock.list, env)
-     }
-     fr <- NaN
-     for(i in 1:length(Symbols)) {
-
-       return.class <- getSymbolLookup()[[Symbols[[i]]]]$return.class
-       return.class <- ifelse(is.null(return.class),default.return.class,
-                              return.class)
-       from <- getSymbolLookup()[[Symbols[[i]]]]$from
-       from <- if(is.null(from)) default.from else from
-       to <- getSymbolLookup()[[Symbols[[i]]]]$to
-       to <- if(is.null(to)) default.to else to
-
-       from.y <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][1])
-       from.m <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][2])-1
-       from.d <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][3])
-       to.y <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][1])
-       to.m <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][2])-1
-       to.d <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][3])
-
-       Symbols.name <- getSymbolLookup()[[Symbols[[i]]]]$name
-       Symbols.name <- ifelse(is.null(Symbols.name),Symbols[[i]],Symbols.name)
-       if(verbose) cat("downloading ",Symbols.name,".....\n\n")
-       Symbols.id <- finam.stock.list[Symbols.name]
-
-       if (is.na(Symbols.id)){
-           if (verbose)
-                cat("Don't know about",Symbols[[i]],"\n\n")
-           next
-       }
-
-       stock.URL <- paste(finam.URL,
-                           "p=", p,
-                           "&em=",Symbols.id,
-                           "&df=",from.d,
-                           "&mf=",from.m,
-                           "&yf=",from.y,
-                           "&dt=",to.d,
-                           "&mt=",to.m,
-                           "&yt=",to.y,
-                           "&cn=",Symbols.name,
-                           sep='')
-       if (verbose) cat(stock.URL);
-       tmp <- tempfile()
-       if (p==1){
-           lts <-  http.get(finam.HOST, paste(stock.URL, '&datf=6', sep=''),  referer='http://www.finam.ru/analysis/export/default.asp', verbose=verbose)
-           write(lts, file=tmp)
-       }else {
-           stock.URL <- paste('http://', finam.HOST, stock.URL, '&datf=1' , sep='')
-           download.file(stock.URL, destfile=tmp, quiet=!verbose)
-       }
-       fr <- read.csv(tmp, as.is=TRUE, colClasses="character")
-       unlink(tmp)
-
-       if(verbose) cat("done.\n")
-       if (p==1){
-            fr <- xts(apply(as.matrix(fr[,(5:6)]),2, as.numeric), as.POSIXct(strptime(paste(fr[,3],fr[,4]), "%Y%m%d %H%M%S")),
-                    src='finam',updated=Sys.time())
-            colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
-                             c('Close','Volume'),
-                             sep='.')
-       }else if (p>7) {
-            fr <- xts(apply(as.matrix(fr[,(5:9)]),2, as.numeric), as.Date(strptime(fr[,3], "%Y%m%d")),
-                 src='finam',updated=Sys.time())
-            colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
-                             c('Open','High','Low','Close','Volume'),
-                             sep='.')
-       }else {
-            fr <- xts(apply(as.matrix(fr[,(5:9)]),2,as.numeric), as.POSIXct(strptime(paste(fr[,3],fr[,4]), "%Y%m%d %H%M%S")),
-                    src='finam',updated=Sys.time())
-            colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
-                             c('Open','High','Low','Close','Volume'),
-                             sep='.')
-       }
-   
-       fr <- convert.time.series(fr=fr,return.class=return.class)
-       if(is.xts(fr) && p>7)
-         indexClass(fr) <- index.class
-
-       Symbols[[i]] <-toupper(gsub('\\^','',Symbols[[i]]))
-       if(auto.assign)
-         assign(Symbols[[i]],fr,env)
-       if(i >= 5 && length(Symbols) > 5) {
-         message("pausing 1 second between requests for more than 5 symbols")
-         Sys.sleep(1)
-       }
-     }
-     if(auto.assign)
-       return(Symbols)
-
-     return(fr)
-
-}
+  function(Symbols,env,return.class='xts',index.class='Date',
+           from='2007-01-01',
+           to=Sys.Date(),
+           adjust=FALSE,
+           period='day',
+           market=NULL,
+           ...)
+  {
+    importDefaults("getSymbols.Finam")
+    this.env <- environment()
+    for(var in names(list(...))) {
+      # import all named elements that are NON formals
+      assign(var, list(...)[[var]], this.env)
+    }
+    
+    default.return.class <- return.class
+    default.from <- from
+    default.to <- to
+    
+    if(missing(verbose)) verbose <- FALSE
+    if(missing(auto.assign)) auto.assign <- FALSE
+    
+    p <- 0
+    
+    if ("tick" == period) p <- 1
+    if ("1min" == period) p <- 2
+    if ("5min" == period) p <- 3
+    if ("10min" == period) p <- 4
+    if ("15min" == period) p <- 5
+    if ("30min" == period) p <- 6
+    if ("hour" == period) p <- 7
+    if ("day" == period) p <- 8
+    if ("week" == period) p <- 9
+    if ("month" == period) p <- 10
+    
+    if (p==0) {
+      message(paste("Unkown period ", period))
+    }	 
+    
+    if (!exists("finam.stock.list")){
+      finam.stock.list <- loadSymbolList(src='Finam')
+      assign('finam.stock.list', finam.stock.list, env)
+    }
+    fr <- NaN
+    for(i in 1:length(Symbols)) {
+      
+      return.class <- getSymbolLookup()[[Symbols[[i]]]]$return.class
+      return.class <- ifelse(is.null(return.class),default.return.class,
+                             return.class)
+      from <- getSymbolLookup()[[Symbols[[i]]]]$from
+      from <- if(is.null(from)) default.from else from
+      to <- getSymbolLookup()[[Symbols[[i]]]]$to
+      to <- if(is.null(to)) default.to else to
+      
+      from.y <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][1])
+      from.m <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][2])-1
+      from.d <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][3])
+      to.y <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][1])
+      to.m <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][2])-1
+      to.d <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][3])
+      
+      Symbols.name <- getSymbolLookup()[[Symbols[[i]]]]$name
+      Symbols.name <- ifelse(is.null(Symbols.name),Symbols[[i]],Symbols.name)
+      if(verbose) cat("downloading ",Symbols.name,".....\n\n")
+      
+      
+      if(!is.null(market))
+      {
+        finam.stock <- finam.stock.list[as.character(finam.stock.list$Symbol) == Symbols.name,]
+        Symbols.id <- finam.stock[finam.stock$Market == market,]$Id
+      }
+      if(is.null(market))
+      {
+        finam.stock <- finam.stock.list[as.character(finam.stock.list$Symbol) == Symbols.name,]
+        Symbols.id <- finam.stock[which.min(finam.stock$Market),]$Id
+        market <- finam.stock[which.min(finam.stock$Market),]$Market
+      }
+      
+      if (length(Symbols.id)==0){
+        if (verbose)
+          cat("Don't know about",Symbols[[i]],"\n\n")
+        next
+      }
+      
+      finam.HOST <- 'export.finam.ru'
+      finam.URL <- "/table.csv?d=d&f=table&e=.csv&dtf=1&tmf=1&MSOR=0&sep=1&sep2=1&at=1&"
+      
+      stock.URL <- paste(finam.URL,
+                         "p=", p,
+                         "&market=",1,
+                         "&em=",Symbols.id,
+                         "&df=",from.d,
+                         "&mf=",from.m,
+                         "&yf=",from.y,
+                         "&dt=",to.d,
+                         "&mt=",to.m,
+                         "&yt=",to.y,
+                         "&cn=",Symbols.name,
+                         sep='')
+      if (verbose) cat(stock.URL);
+      tmp <- tempfile()
+      if (p==1){
+        lts <-  http.get(finam.HOST, paste(stock.URL, '&datf=6', sep=''),  referer='http://www.finam.ru/analysis/export/default.asp', verbose=verbose)
+        write(lts, file=tmp)
+      }else {
+        stock.URL <- paste('http://', finam.HOST, stock.URL, '&datf=1' , sep='')
+        download.file(stock.URL, destfile=tmp, quiet=!verbose)
+      }
+      fr <- read.csv(tmp, as.is=TRUE, colClasses="character")
+      unlink(tmp)
+      
+      if(verbose) cat("done.\n")
+      if (p==1){
+        fr <- xts(apply(as.matrix(fr[,(5:6)]),2, as.numeric), as.POSIXct(strptime(paste(fr[,3],fr[,4]), "%Y%m%d %H%M%S")),
+                  src='finam',updated=Sys.time())
+        colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
+                              c('Close','Volume'),
+                              sep='.')
+      }else if (p>7) {
+        fr <- xts(apply(as.matrix(fr[,(5:9)]),2, as.numeric), as.Date(strptime(fr[,3], "%Y%m%d")),
+                  src='finam',updated=Sys.time())
+        colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
+                              c('Open','High','Low','Close','Volume'),
+                              sep='.')
+      }else {
+        fr <- xts(apply(as.matrix(fr[,(5:9)]),2,as.numeric), as.POSIXct(strptime(paste(fr[,3],fr[,4]), "%Y%m%d %H%M%S")),
+                  src='finam',updated=Sys.time())
+        colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
+                              c('Open','High','Low','Close','Volume'),
+                              sep='.')
+      }
+      
+      fr <- convert.time.series(fr=fr,return.class=return.class)
+      if(is.xts(fr) && p>7)
+        indexClass(fr) <- index.class
+      
+      Symbols[[i]] <-toupper(gsub('\\^','',Symbols[[i]]))
+      if(auto.assign)
+        assign(Symbols[[i]],fr,env)
+      if(i >= 5 && length(Symbols) > 5) {
+        message("pausing 1 second between requests for more than 5 symbols")
+        Sys.sleep(1)
+      }
+    }
+    if(auto.assign)
+      return(Symbols)
+    
+    return(fr)
+  }
 
 
 "getSymbols.mfd" <-
@@ -587,39 +601,6 @@ fr <- xts(apply(as.matrix(fr[,(5:10)]),2, as.numeric), as.Date(strptime(fr[,3], 
 	 
 	 if(exists('fr'))  
 		return(fr)
-}
-
-"loadStockList" <-
-function (verbose = FALSE){
-    stocklist.URL = 'https://www.finam.ru/cache/icharts/icharts.js'
-	tmp <- tempfile()
-	curl_download(stocklist.URL, tmp, quiet=!verbose)
-    fr <- readLines(con = tmp, warn=FALSE)
-    unlink(tmp)
-    ids <- sub("var .*?= \\[", "", fr[1])
-    ids <- sub("\\];", "", ids)
-    ids <- strsplit(ids, ",")
-	
-	markets <- sub("var .*?= \\[", "", fr[4])
-    markets <- sub("\\];", "", markets)
-    markets <- strsplit(markets, ",")
-
-    names <- sub("var .*?= \\[", "", fr[3])
-    names <- sub("\\];", "", names)
-    names <- gsub("'", "", names)
-    names <- strsplit(names, ",")
-	names[[1]]->names
-	names[-(11414)]->names
-    res <- unlist(ids)	
-	markets<-unlist(markets)
-	
-	
-	data<-data.frame(names[1:min(length(res),length(names))],res[1:min(length(res),length(names))],markets[1:min(length(res),length(names))])
-	data[data[,3]!=3,]->data
-	rbind(data[(data[,1]%in%data[data[,3]==1,1]) & data[,3]==1,],data[!(data[,1]%in%data[data[,3]==1,1]),])->data
-	data[,2]->res
-    names(res) <- data[,1]
-    return(res)
 }
 
 #This one is taken from quanmod package since it's not available through the API
--- R/loadSymbolList.r
+++ R/loadSymbolList.r
@@ -1,117 +1,152 @@
 
-
-
-#function retrives All avalible instruments of exchange
-"loadSymbolList" <- function #S3 function (Poloniex is a class of first argument)
-(src='poloniex',
- verbose=FALSE,
- auto.assign=FALSE,
- ...)
-{
-        importDefaults("loadSymbolList"); #rewrite default values if specified by setDefaults
-        env <- environment()
-		src <- tolower(src)
-  
-  ## choose exchange       
-  if(src == 'kraken')
-	downloadUrl <- 'https://api.kraken.com/0/public/AssetPairs'
-
-  if(src == 'poloniex')		
-	downloadUrl <- 'https://poloniex.com/public?command=returnTicker'
-						
-  if (src == "binance")
-    downloadUrl <- "https://api.binance.com/api/v1/ticker/allPrices"
-
-  if (src == "bitfinex")
-    downloadUrl <- "https://api.bitfinex.com/v1/symbols"
-
-  if (src == "bitstamp")
-    downloadUrl <- "https://www.bitstamp.net/api/v2/trading-pairs-info/"
-
-  if (src == "bttrex")
-    downloadUrl <- "https://bittrex.com/api/v1.1/public/getmarkets"
-
-  if (src == "cex")
-    downloadUrl <- "https://cex.io/api/currency_limits"
-
-  if (src == "gate")
-    downloadUrl <- "http://data.gate.io/api2/1/pairs"
-
-  if (src == "gatecoin")
-    downloadUrl <- "https://api.gatecoin.com/Public/LiveTickers"
-
-  if (src == "gdax")
-    downloadUrl <- "https://api.gdax.com/products/"
-
-  if(src == "gemini")
-    downloadUrl <- "https://api.gemini.com/v1/symbols"
-	
-  if (src == "hitbtc")
-    downloadUrl <- "https://api.hitbtc.com/api/2/public/symbol"
-
-  if (src == "liqui")
-    downloadUrl <- "https://api.liqui.io/api/3/info"
-
-  if (src == "lykke") 
-    downloadUrl <- "https://hft-api.lykke.com/api/AssetPairs"
-
-  if (src == "xbtce") 
-    downloadUrl <- "https://cryptottlivewebapi.xbtce.net:8443/api/v1/public/symbol"
-
-#download and parse data			
-rawdata_m <- jsonlite::fromJSON(downloadUrl, simplifyVector = TRUE)			
-
-if (src == "liqui")	
-	{
-	rawdata_m <- t(sapply(rawdata_m$pairs,cbind))
-	rawdata_m <- data.table(rownames(rawdata_m),rawdata_m)
-	names(rawdata_m)[1] <- 'Symbol'
-	}
-if (src == "gatecoin")		
-	{
-	rawdata_m <- rawdata_m$tickers
-	names(rawdata_m)[1] <- 'Symbol'
-	}
-if (src == "cex")
-	{
-	rawdata_m <- rawdata_m$data$pairs
-	rawdata_m$Symbol <- paste0(rawdata_m[,1], "/", rawdata_m[,2])
-	}
-if (src == "bttrex")
-	{
-	rawdata_m <- rawdata_m$result	
-	names(rawdata_m)[6] <- 'Symbol'
-	}
-if (src == "kraken")	
-	{
-	rawdata_m <- data.table(t(sapply(rawdata_m$result,function(x) t(data.frame(x[c(1:9,14:15)])))))
-	names(rawdata_m)[1] <- 'Symbol'
-	}
-if (src == "poloniex")	
-	{
-	colnms <- names(rawdata_m[[1]])
-	rownms <- names(rawdata_m)
-	rawdata_m <- (t(sapply(rawdata_m,cbind)))
-	rawdata_m <- data.table(rownms,rawdata_m)
-	names(rawdata_m) <- c('Symbol',colnms)
-	}
- if (src == "binance")
-    names(rawdata_m) <- c('Symbol','Price')
- if (src %in% c("gate","gemini"))	
-	{
-	rawdata_m <- data.table(rawdata_m)
-	names(rawdata_m) <- 'Symbol'
-	}
-if (src %in% c("gdax","hitbtc","lykke"))		
-	names(rawdata_m)[1]<- 'Symbol'
-
-
-	
-result <-paste('symbol_list_',toupper(gsub('\\^','',src)),sep='_')			
-if(auto.assign)                
-	{
-    assign(result, rawdata_m,globalenv())			
-	return(result)
-	}										    
-return(rawdata_m)
-}
+ #function retrives All avalible instruments of exchange
+ "loadSymbolList" <- function 
+ (src='poloniex',
+  verbose=FALSE,
+  auto.assign=FALSE,
+  ...)
+ {
+   importDefaults("loadSymbolList"); #rewrite default values if specified by setDefaults
+   env <- environment()
+   src <- tolower(src)
+   
+   ## choose exchange       
+   if(src == 'kraken')
+     downloadUrl <- 'https://api.kraken.com/0/public/AssetPairs'
+   
+   if(src == 'poloniex')		
+     downloadUrl <- 'https://poloniex.com/public?command=returnTicker'
+   
+   if (src == "binance")
+     downloadUrl <- "https://api.binance.com/api/v1/ticker/allPrices"
+   
+   if (src == "bitfinex")
+     downloadUrl <- "https://api.bitfinex.com/v1/symbols"
+   
+   if (src == "bitstamp")
+     downloadUrl <- "https://www.bitstamp.net/api/v2/trading-pairs-info/"
+   
+   if (src == "bttrex")
+     downloadUrl <- "https://bittrex.com/api/v1.1/public/getmarkets"
+   
+   if (src == "cex")
+     downloadUrl <- "https://cex.io/api/currency_limits"
+   
+   if (src == "gate")
+     downloadUrl <- "http://data.gate.io/api2/1/pairs"
+   
+   if (src == "gatecoin")
+     downloadUrl <- "https://api.gatecoin.com/Public/LiveTickers"
+   
+   if (src == "gdax")
+     downloadUrl <- "https://api.gdax.com/products/"
+   
+   if(src == "gemini")
+     downloadUrl <- "https://api.gemini.com/v1/symbols"
+   
+   if (src == "hitbtc")
+     downloadUrl <- "https://api.hitbtc.com/api/2/public/symbol"
+   
+   if (src == "liqui")
+     downloadUrl <- "https://api.liqui.io/api/3/info"
+   
+   if (src == "lykke") 
+     downloadUrl <- "https://hft-api.lykke.com/api/AssetPairs"
+   
+   if (src == "xbtce") 
+     downloadUrl <- "https://cryptottlivewebapi.xbtce.net:8443/api/v1/public/symbol"
+   
+   if(src == 'finam')
+     downloadUrl <- 'https://finam.ru/cache/N72Hgd54/icharts/icharts.js'
+   
+   
+   #download data		
+   if (src != "finam")	
+     rawdata_m <- jsonlite::fromJSON(downloadUrl, simplifyVector = TRUE)		
+   if (src == "finam")	
+   {
+     tmp <- tempfile()
+     download.file(downloadUrl, destfile = tmp,method = "libcurl")
+   }
+   
+   
+   # clean data
+   if (src == "finam")	
+   {
+     fr <- readLines(con = tmp, warn = FALSE)
+     unlink(tmp)
+     EmitentIds <- sub("var .*?= \\[", "", fr[1])
+     EmitentIds <- sub("\\];", "", EmitentIds)
+     EmitentIds <- strsplit(EmitentIds, ",")
+     EmitentNames <- sub("var .*?= \\[", "", fr[2])
+     EmitentNames <- sub("\\];", "", EmitentNames)
+     EmitentNames <- strsplit(EmitentNames, "','")
+     EmitentCodes <- sub("var .*?= \\[", "", fr[3])
+     EmitentCodes <- sub("\\];", "", EmitentCodes)
+     EmitentCodes <- strsplit(EmitentCodes, "','")
+     EmitentMarkets <- sub("var .*?= \\[", "", fr[4])
+     EmitentMarkets <- sub("\\];", "", EmitentMarkets)
+     EmitentMarkets <- strsplit(EmitentMarkets, ",")
+     EmitentUrls <- sub("var .*?= \\{", "", fr[9])
+     EmitentUrls <- sub("\\];", "", EmitentUrls)
+     EmitentUrls <- strsplit(EmitentUrls, ",")
+     
+     rawdata_m <- data.table(as.character(EmitentCodes[[1]]),as.character(EmitentNames[[1]]),as.character(EmitentIds[[1]]),as.character(EmitentMarkets[[1]]),as.character(EmitentUrls[[1]]))
+     names(rawdata_m) <- c('Symbol','Name','Id','Market','Url')
+     rm(list = c('EmitentIds','EmitentCodes','EmitentNames','EmitentMarkets','EmitentUrls','fr'))
+   }
+   
+   if (src == "liqui")	
+   {
+     rawdata_m <- t(sapply(rawdata_m$pairs,cbind))
+     rawdata_m <- data.table(rownames(rawdata_m),rawdata_m)
+     names(rawdata_m)[1] <- 'Symbol'
+   }
+   if (src == "gatecoin")		
+   {
+     rawdata_m <- rawdata_m$tickers
+     names(rawdata_m)[1] <- 'Symbol'
+   }
+   if (src == "cex")
+   {
+     rawdata_m <- rawdata_m$data$pairs
+     rawdata_m$Symbol <- paste0(rawdata_m[,1], "/", rawdata_m[,2])
+   }
+   if (src == "bttrex")
+   {
+     rawdata_m <- rawdata_m$result	
+     names(rawdata_m)[6] <- 'Symbol'
+   }
+   if (src == "kraken")	
+   {
+     rawdata_m <- data.table(t(sapply(rawdata_m$result,function(x) t(data.frame(x[c(1:9,14:15)])))))
+     names(rawdata_m)[1] <- 'Symbol'
+   }
+   if (src == "poloniex")	
+   {
+     colnms <- names(rawdata_m[[1]])
+     rownms <- names(rawdata_m)
+     rawdata_m <- (t(sapply(rawdata_m,cbind)))
+     rawdata_m <- data.table(rownms,rawdata_m)
+     names(rawdata_m) <- c('Symbol',colnms)
+   }
+   if (src == "binance")
+     names(rawdata_m) <- c('Symbol','Price')
+   if (src %in% c("gate","gemini"))	
+   {
+     rawdata_m <- data.table(rawdata_m)
+     names(rawdata_m) <- 'Symbol'
+   }
+   if (src %in% c("gdax","hitbtc","lykke"))		
+     names(rawdata_m)[1]<- 'Symbol'
+   
+   
+   
+   result <-paste('symbol_list_',toupper(gsub('\\^','',src)),sep='_')			
+   if(auto.assign)                
+   {
+     assign(result, rawdata_m,globalenv())			
+     return(result)
+   }										    
+   return(rawdata_m)
+ }
