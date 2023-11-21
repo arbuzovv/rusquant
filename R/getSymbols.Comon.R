50199b043ad2177cca28e3f0f3712d28fe9aad03
Vyacheslav Arbuzov <arbuzov1989@gmail.com>
2023-11-21 16:24:43 +0500

    update docs

--- R/getSymbols.Comon.R
+++ R/getSymbols.Comon.R
@@ -1,13 +1,13 @@
-#' @title Download Alor data
+#' @title Download data from Comon copytrading platform
 #'
-#' @description Download historical market data from Alor for a given symbol and time range.
+#' @description Download historical market data from Comon for a given trader id
 #'
 #' @param Symbols a character id of strategy on Comon
 #' @param env environment where to create the downloaded data object.
-#' @param period a character string indicating the frequency of the data to download. Possible values are '1min', '5min', 'hour', 'day', 'week', and 'month'.
+#' @param period a character string indicating the frequency of the data to download. Possible values are 'day'.
 #' @param verbose a logical indicating whether to print the response details or not.
 #' @param auto.assign a logical indicating whether to automatically assign the downloaded data to the global environment.
-#' @param ... additional arguments passed to getSymbols.Alor
+#' @param ... additional arguments passed to getSymbols.Comon
 #' @return returns an data.table object containing financial data
 #' @note Not for the faint of heart. All profits and losses related are yours and yours alone. If you don't like it, write it yourself.
 #' @author Vyacheslav Arbuzov
